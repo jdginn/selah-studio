c961e05859b680566c7c2d2b4800324086b3993b
Justin Ginn <Justindavid.ginn@gmail.com>
2024-05-25 11:39:18 -0400

    WIP

--- selah.py
+++ selah.py
@@ -45,35 +45,40 @@ class Wall:
         if meshv is None:
             raise RuntimeError
         vertices = [v for v in meshv.iter()]
-        self.vertices = np.ndarray(shape=(len(vertices), 3))
+        self.vertices = np.ndarray(shape=(len(vertices) - 1, 3))
         for i, v in enumerate(vertices[1:]):
             self.vertices[i] = [v.get("x"), v.get("y"), v.get("z")]
         mesht = mesh.find("schema:triangles", namespace)
         if mesht is None:
             raise RuntimeError
         triangles = [t for t in mesht.iter()]
-        self.triangles = np.ndarray(shape=(len(triangles), 3))
+        self.triangles = np.ndarray(shape=(len(triangles) - 1, 3))
         for i, t in enumerate(triangles[1:]):
             self.triangles[i] = [t.get("v1"), t.get("v2"), t.get("v3")]
 
     def simplify(self):
         # TODO: make this take the simplifier as a closure
-        simplifier = pyqfmr.Simplify()
+        simplifier = pyfqmr.Simplify()
         simplifier.setMesh(self.vertices, self.triangles)
         simplifier.simplify_mesh(
-            target_count=100, aggressiveness=8, preserve_border=True, verbose=10
+            target_count=60, aggressiveness=100, preserve_border=True, verbose=10
         )
         self.vertices, self.triangles, _ = simplifier.getMesh()
 
 
 def build_room(walls: typing.List[Wall]) -> pra.Room:
     reduc = 1000.0
-    mat = pra.Material(energy_absorption=0.2, scattering=0.1)
+    mat = pra.Material(energy_absorption=0.2, scattering=0.001)
     _walls = []
     for w in walls:
+        # w.simplify()
         for tri in w.triangles:
             corner = np.array(
-                [w.vertices[tri[0]], w.vertices[tri[1]], w.vertices[tri[2]]],
+                [
+                    w.vertices[int(tri[0])],
+                    w.vertices[int(tri[1])],
+                    w.vertices[int(tri[2])],
+                ],
                 dtype=np.float32,
             )
             _walls.append(
@@ -86,15 +91,15 @@ def build_room(walls: typing.List[Wall]) -> pra.Room:
     return pra.Room(
         _walls,
         fs=8000,
-        max_order=3,
-        ray_tracing=True,
-        air_absorption=True,
+        max_order=2,
+        ray_tracing=False,
+        air_absorption=False,
     )
 
 
 class Room:
     reduc = 1000.0
-    mat = pra.Material(energy_absorption=0.2, scattering=0.1)
+    mat = pra.Material(energy_absorption=0.1, scattering=0.1)
 
     def __init__(self, walls: typing.List[Wall]):
         self._walls = []
@@ -107,52 +112,13 @@ class Room:
                 )
                 self._walls.append(
                     pra.Wall(
-                        corner.T / Room.reduc,
+                        corner / Room.reduc,
                         Room.mat.energy_absorption["coeffs"],
                         Room.mat.scattering["coeffs"],
                     )
                 )
 
         pass
-
-    # reduc = 1000.0
-    #
-    # material = pra.Material(energy_absorption=0.2, scattering=0.1)
-    # walls = []
-    # # for w in range(ntriang):
-    # #     print(the_mesh.vectors[w].T / reduc)
-    # #     walls.append(
-    # #         pra.wall_factory(
-    # #             the_mesh.vectors[w].T / reduc,
-    # #             material.energy_absorption["coeffs"],
-    # #             material.scattering["coeffs"],
-    # #         )
-    # #     )
-    # for f in faces:
-    #     corner = np.array(
-    #         [vertices[f[0]], vertices[f[1]], vertices[f[2]]],
-    #         dtype=np.float32,
-    #     )
-    #     # IPython.embed()
-    #     walls.append(
-    #         pra.wall_factory(
-    #             corner.T / reduc,
-    #             material.energy_absorption["coeffs"],
-    #             material.scattering["coeffs"],
-    #         )
-    #     )
-    #
-    # room = (
-    #     pra.Room(
-    #         walls,
-    #         fs=8000,
-    #         max_order=3,
-    #         ray_tracing=True,
-    #         air_absorption=True,
-    #     )
-    #     .add_source([0.1, 0.1, 0.5])
-    #     .add_microphone_array(np.c_[[1.0, 1.0, 0.4]])
-    # )
 
 
 if __name__ == "__main__":
@@ -174,23 +140,27 @@ if __name__ == "__main__":
         raise RuntimeError
     objects = [Wall(o) for o in res.findall("schema:object", namespace)]
     room = build_room(objects)
-    room.add_source([0.1, 0.1, 0.5]).add_microphone_array(np.c_[[1.0, 1.0, 0.4]])
+    room.add_source([3.3, 4.0, 1.8]).add_microphone_array(np.c_[[2.5, 2.2, 1.8]])
+    # room.add_source([1.0, 1.0, 1.0]).add_microphone_array(np.c_[[0.8, 0.8, 0.8]])
+    # IPython.embed()
 
     # compute the rir
     room.image_source_model()
     room.ray_tracing()
     room.compute_rir()
-    for s in room.sources:
-        # s.set_ordering("strongest")
-        for i in s.images:
-            print("image:")
-            print(i)
+    # for s in room.sources:
+    #     # s.set_ordering("strongest")
+    #     for i in s.images:
+    #         print("image:")
+    #         print(i)
     room.plot_rir()
-    # IPython.embed()
-
-    # # show the room
-    room.plot(img_order=1, mic_marker_size=25)
-    plt.ylim(0, 10)
-    plt.xlim(0, 10)
-    plt.savefig("imgs/stl_room.png")
+    plt.xlim(0, 150)
+    plt.savefig("imgs/stl_rir_plot.png")
     plt.show()
+
+    # # # show the room
+    # room.plot(img_order=1, mic_marker_size=25)
+    # plt.ylim(0, 10)
+    # plt.xlim(0, 10)
+    # plt.savefig("imgs/stl_room.png")
+    # plt.show()
