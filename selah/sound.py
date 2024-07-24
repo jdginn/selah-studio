ad4a30cc6d728093392ee7cdfe964d174a5ff0ea
Justin Ginn <Justindavid.ginn@gmail.com>
2024-07-24 12:03:34 +0200

    sound: add docstrings

--- selah/sound.py
+++ selah/sound.py
@@ -4,8 +4,14 @@ SPEED_OF_SOUND = 343.0
 
 
 def db(gain: float) -> float:
+    """
+    Converts a raw gain into decibels
+    """
     return 10 * math.log10(gain)
 
 
 def from_db(gain: float) -> float:
+    """
+    Converts from decibels to raw gain
+    """
     return math.pow(10, gain / 10)
