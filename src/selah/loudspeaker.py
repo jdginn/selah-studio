loudspeaker: fix rotation point
Instead of referencing all internal calculations from (0, 0, 0),
reference from the position of the acoustic axis and perform any
rotations perpendicular to that axis.

Also, use the proper Loudspeaker in more places, instead of the old
LoudspeakerSpec approach.

src/selah/loudspeaker.py
@@ -23,13 +23,13 @@ class LoudspeakerSpec:
     # Takes arguments mapping degrees to gain in dB
     def __init__(
         self,
-        horiz_disp: dict[float, float] = {0: 0, 30: 0, 60: -12, 70: -100},
-        vert_disp: dict[float, float] = {0: 0, 30: -9, 60: -15, 70: -19, 80: -30},
         x_dim: float = 0.520,
         y_dim: float = 0.256,
         z_dim: float = 0.380,
         y_offset: float = 0.128,
         z_offset: float = 0.128,
+        horiz_disp: dict[float, float] = {0: 0, 30: 0, 60: -12, 70: -100},
+        vert_disp: dict[float, float] = {0: 0, 30: -9, 60: -15, 70: -19, 80: -30},
         x_margin: float = 0.05,
         y_margin: float = 0.05,
         z_margin: float = 0.05,
@@ -88,8 +88,8 @@ class Loudspeaker:
     def __init__(
         self,
         spec: LoudspeakerSpec,
-        position: typing.Union[npt.NDArray, list[float]],
-        normal: typing.Union[npt.NDArray, list[float]],
+        position: typing.Union[npt.NDArray, list[float]] = np.array([0, 0, 0]),
+        normal: typing.Union[npt.NDArray, list[float]] = np.array([-1, 0, 0]),
     ):
         """
         Loudspeaker represents a specific louspeaker at a specific location in space.
@@ -120,14 +120,18 @@ class Loudspeaker:
         """
         extents = np.array([self.spec._x_dim, self.spec._y_dim, self.spec._z_dim])
         mesh = trimesh.primitives.Box(np.array(extents))
-        mesh.apply_translation(extents / 2)
-
-        angle = trimesh.transformations.angle_between_vectors(
-            np.array([1, 0, 0]), self.normal
+        mesh.apply_translation(
+            extents / 2 - [0, self.spec._y_offset, self.spec._z_offset]
         )
-        axis = np.cross(np.array([1, 0, 0]), self.normal)
-        rotation_matrix = trimesh.transformations.rotation_matrix(angle, axis)
-        mesh.apply_transform(rotation_matrix)
+
+        if not np.allclose(self.normal, np.array([-1, 0, 0])):
+            angle = trimesh.transformations.angle_between_vectors(
+                np.array([-1, 0, 0]), self.normal
+            )
+            axis = np.cross(np.array([-1, 0, 0]), self.normal)
+            rotation_matrix = trimesh.transformations.rotation_matrix(angle, axis)
+            mesh.apply_transform(rotation_matrix)
+        mesh.apply_translation(self.position)
 
         mesh.visual.vertex_colors = tv.random_color()  # pyright: ignore
         return mesh
@@ -201,8 +205,6 @@ class Loudspeaker:
     def test_intersection(
         self,
         test_mesh: trimesh.Trimesh,
-        placement: typing.Union[npt.NDArray, list[float]] = np.array([0, 0, 0]),
-        norm: typing.Union[None, npt.NDArray, list[float]] = None,
     ) -> bool:
         """
         Returns True if this loudspeaker intersects another mesh.
@@ -214,19 +216,7 @@ class Loudspeaker:
         norm:       vector describing the direction of this loudspeaker is pointed
         """
         # TODO:
-        if isinstance(placement, list):
-            placement = np.array(placement)
-        if isinstance(norm, list):
-            norm = np.array(placement)
-        mesh = self.mesh.copy()
-        if norm is not None:
-            angle = trimesh.transformations.angle_between_vectors(
-                np.array([1, 0, 0]), norm
-            )
-            axis = np.cross(np.array([1, 0, 0]), norm)
-            rotation_matrix = trimesh.transformations.rotation_matrix(angle, axis)
-            mesh.apply_transform(rotation_matrix)
-        contained_points = test_mesh.contains(mesh.vertices)
+        contained_points = test_mesh.contains(self.mesh.vertices)
 
         # NOTE: it would seem like we could take a shortcut here and return False
         # if no points are contaiend. However, that will not correctly handle the
@@ -235,7 +225,7 @@ class Loudspeaker:
         intersection = False
         # Check whether each vertex intersects the mesh
         pq = prox.ProximityQuery(test_mesh)
-        points_on_surface, distance_to_surface, _ = pq.on_surface(mesh.vertices)
+        points_on_surface, distance_to_surface, _ = pq.on_surface(self.mesh.vertices)
         for i, dist in enumerate(distance_to_surface):
             if dist == 0:
                 intersection = True

src/selah/room.py
@@ -13,7 +13,7 @@ from .exceptions import SelahException
 from .material import MaterialManager
 from .loudspeaker import Loudspeaker, LoudspeakerSpec
 from .sound import SPEED_OF_SOUND, db, from_db
-from .source import Source, Shot, Reflection, ReflectionException, ShotException
+from .source import Source, Shot, Reflection, ReflectionException
 from .wall import Axis, Wall, build_wall_from_point
 
 
@@ -53,7 +53,7 @@ class ListeningTriangle:
         height: float,
         dist_from_wall: float,
         dist_from_center: float,
-        source: LoudspeakerSpec,
+        source_spec: LoudspeakerSpec,
         rfz_radius: float,
         **kwargs,
     ) -> None:
@@ -62,7 +62,7 @@ class ListeningTriangle:
         self.speaker_height = kwargs.get("speaker_height", height)
         self.dist_from_wall = dist_from_wall
         self.dist_from_center = dist_from_center
-        self.source = source
+        self.source_spec = source_spec
         self.rfz_radius = rfz_radius
         self._deviation = kwargs.get("deviation", 0)
 
@@ -77,12 +77,12 @@ class ListeningTriangle:
 
         # TODO: need to know which direction from the wall is interior vs exterior
 
-    def l_source(self) -> npt.NDArray:
+    def l_source(self) -> Loudspeaker:
         """Returns the position of the left stereo source"""
         p = self._wall.center_pos()
         match self._axis:
             case Axis.X:
-                return np.array(
+                speaker_pos = np.array(
                     [
                         self._wall_pos + self.dist_from_wall,
                         p[1] - self.dist_from_center,
@@ -94,13 +94,18 @@ class ListeningTriangle:
                 raise RuntimeError
             case Axis.Z:
                 raise RuntimeError
+        return Loudspeaker(
+            self.source_spec,
+            speaker_pos,
+            geometry.dir_from_points(speaker_pos, self.listening_pos()),
+        )
 
-    def r_source(self) -> npt.NDArray:
+    def r_source(self) -> Loudspeaker:
         """Returns the position of the right stereo source"""
         p = self._wall.center_pos()
         match self._axis:
             case Axis.X:
-                return np.array(
+                speaker_pos = np.array(
                     [
                         self._wall_pos + self.dist_from_wall,
                         p[1] + self.dist_from_center,
@@ -112,6 +117,11 @@ class ListeningTriangle:
                 raise RuntimeError
             case Axis.Z:
                 raise RuntimeError
+        return Loudspeaker(
+            self.source_spec,
+            speaker_pos,
+            geometry.dir_from_points(speaker_pos, self.listening_pos()),
+        )
 
     # Value from Rod Gervais' book Home Recording Studio: Build It Like The Pros
     LISTENER_DIST_INTO_TRIANGLE = 0.38
@@ -180,17 +190,8 @@ class Room:
             rfz_radius,
             **kwargs,
         )
-        listen_pos = self._lt.listening_pos()
-        l_source = Loudspeaker(
-            source,
-            self._lt.l_source(),
-            geometry.dir_from_points(self._lt.l_source(), listen_pos),
-        )
-        r_source = Loudspeaker(
-            source,
-            self._lt.r_source(),
-            geometry.dir_from_points(self._lt.r_source(), listen_pos),
-        )
+        l_source = self._lt.l_source()
+        r_source = self._lt.r_source()
 
         if l_source.test_intersection(self.mesh):
             raise CollisionException(
@@ -461,7 +462,6 @@ class Room:
     def trace_arrivals(
         self,
         source: Loudspeaker,
-        source_pos: npt.NDArray,
         listen_pos: npt.NDArray,
         **kwargs,
     ) -> typing.List[Source]:
@@ -486,7 +486,7 @@ class Room:
             arrival, intersects_rfz = self.trace_shot(
                 mesh,
                 shot,
-                source_pos,
+                source.position,
                 listen_pos,
                 self._lt.rfz_radius,
                 order,
@@ -505,10 +505,16 @@ class Room:
         Plots a 2-dimensional representation of the room as viewed from above.
         """
         plt.scatter(
-            self._lt.l_source()[0], self._lt.l_source()[1], marker="x", linewidth=8
+            self._lt.l_source().position[0],
+            self._lt.l_source().position[1],
+            marker="x",
+            linewidth=8,
         )
         plt.scatter(
-            self._lt.r_source()[0], self._lt.r_source()[1], marker="x", linewidth=8
+            self._lt.r_source().position[0],
+            self._lt.r_source().position[1],
+            marker="x",
+            linewidth=8,
         )
         circle = patches.Circle(
             (self._lt.listening_pos()[0], self._lt.listening_pos()[1]),
@@ -532,10 +538,16 @@ class Room:
         Plots a 2-dimensional representation of the room as viewed from the side.
         """
         plt.scatter(
-            self._lt.l_source()[0], self._lt.l_source()[2], marker="x", linewidth=8
+            self._lt.l_source().position[0],
+            self._lt.l_source().position[2],
+            marker="x",
+            linewidth=8,
         )
         plt.scatter(
-            self._lt.r_source()[0], self._lt.r_source()[2], marker="x", linewidth=8
+            self._lt.r_source().position[0],
+            self._lt.r_source().position[2],
+            marker="x",
+            linewidth=8,
         )
         circle = patches.Circle(
             (self._lt.listening_pos()[0], self._lt.listening_pos()[2]),

test/test_loudspeaker.py
@@ -4,10 +4,11 @@ import pytest
 
 import numpy as np
 import numpy.typing as npt
+from numpy.testing import assert_allclose
 import trimesh
 import trimesh.visual as tv
 
-from selah.loudspeaker import Loudspeaker
+from selah.loudspeaker import Loudspeaker, LoudspeakerSpec
 
 
 def box_from_origin(
@@ -21,40 +22,101 @@ def box_from_origin(
     return box
 
 
+def show_scene(items: list[trimesh.Trimesh]):
+    s = trimesh.Scene()
+    for item in items:
+        s.add_geometry(item)
+    s.show()
+
+
+def test_loudspeaker_position():
+    base = LoudspeakerSpec(x_dim=2, y_dim=2, z_dim=2, y_offset=1, z_offset=1)
+
+    assert np.allclose(
+        Loudspeaker(base).mesh.vertices,
+        np.array(
+            [
+                [0, -1, -1],
+                [0, -1, 1],
+                [0, 1, -1],
+                [0, 1, 1],
+                [2, -1, -1],
+                [2, -1, 1],
+                [2, 1, -1],
+                [2, 1, 1],
+            ]
+        ),
+    )
+
+    assert np.allclose(
+        Loudspeaker(base, position=[1, 1, 1]).mesh.vertices,
+        np.array(
+            [
+                [1, 0, 0],
+                [1, 0, 2],
+                [1, 2, 0],
+                [1, 2, 2],
+                [3, 0, 0],
+                [3, 0, 2],
+                [3, 2, 0],
+                [3, 2, 2],
+            ]
+        ),
+    )
+
+    assert np.allclose(
+        Loudspeaker(base, normal=[0, 1, 0]).mesh.vertices,
+        np.array(
+            [
+                [-1, 0, -1],
+                [-1, 0, 1],
+                [1, 0, -1],
+                [1, 0, 1],
+                [-1, -2, -1],
+                [-1, -2, 1],
+                [1, -2, -1],
+                [1, -2, 1],
+            ]
+        ),
+    )
+
+
 def test_test_intersection_mesh():
+    base = LoudspeakerSpec(2, 2, 2, y_offset=1, z_offset=1)
+
     # Speaker outside room
-    assert not Loudspeaker(
-        x_dim=2, y_dim=2, z_dim=2, y_offset=1, z_offset=1
-    ).test_intersection(box_from_origin([1, 1, 1]), placement=[4, 4, 4])
+    assert not Loudspeaker(base, position=[4, 4, 4]).test_intersection(
+        box_from_origin([1, 1, 1])
+    )
 
     # Speaker fully inside room
-    assert not Loudspeaker(
-        x_dim=2, y_dim=2, z_dim=2, y_offset=1, z_offset=1
-    ).test_intersection(box_from_origin([10, 10, 10]), placement=[3, 3, 3])
+    assert not Loudspeaker(base, position=[3, 3, 3]).test_intersection(
+        box_from_origin([10, 10, 10])
+    )
 
     # Speaker sharing one wall with room
-    assert Loudspeaker(
-        x_dim=2, y_dim=2, z_dim=2, y_offset=1, z_offset=1
-    ).test_intersection(box_from_origin([10, 10, 10]), placement=[0, 2, 2])
+    assert Loudspeaker(base, position=[0, 2, 2]).test_intersection(
+        box_from_origin([10, 10, 10])
+    )
 
     # Speaker same dimensions as room
     assert Loudspeaker(
-        x_dim=10, y_dim=10, z_dim=10, y_offset=5, z_offset=5
-    ).test_intersection(box_from_origin([10, 10, 10]), placement=[0, 0, 0])
+        LoudspeakerSpec(10, 10, 10, y_offset=5, z_offset=5)
+    ).test_intersection(box_from_origin([10, 10, 10]))
 
     # Speaker partially outside room
-    assert Loudspeaker(
-        x_dim=2, y_dim=2, z_dim=2, y_offset=1, z_offset=1
-    ).test_intersection(box_from_origin([10, 10, 10]), placement=[0, 9, 9])
+    assert Loudspeaker(base, position=[0, 9, 9]).test_intersection(
+        box_from_origin([10, 10, 10])
+    )
 
     # One edge of speaker rotated 45 degrees intersects wall
     # First, if we don't rotate, we don't hit the wall
+    # For this test, rotate around a corner rather than an axis in the middle of X face
     assert not Loudspeaker(
-        x_dim=2, y_dim=2, z_dim=2, y_offset=1, z_offset=1
-    ).test_intersection(box_from_origin([10, 10, 10]), placement=[3, 3, 8.8])
+        LoudspeakerSpec(2, 2, 2), position=[7.8, 3, 3]
+    ).test_intersection(box_from_origin([10, 10, 10]))
+
     # Now rotate 45deg and one edge intersects
     assert Loudspeaker(
-        x_dim=2, y_dim=2, z_dim=2, y_offset=1, z_offset=1
-    ).test_intersection(
-        box_from_origin([10, 10, 10]), placement=[3, 3, 8.8], norm=[1, 1, 0]
-    )
+        LoudspeakerSpec(2, 2, 2), position=[7.8, 3, 3], normal=[-1, 1, 0]
+    ).test_intersection(box_from_origin([10, 10, 10]))