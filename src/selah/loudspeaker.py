loudspeaker: fix rotation
Correctly handles rotating to normals with an X-axis component

Caveat: Requires a separate path for normals with no X-axis component

src/selah/loudspeaker.py
@@ -121,18 +121,34 @@ class Loudspeaker:
         if not hasattr(self, "_mesh"):
             extents = np.array([self.spec._x_dim, self.spec._y_dim, self.spec._z_dim])
             mesh = trimesh.primitives.Box(np.array(extents))
-            mesh.apply_translation(
-                extents / 2 - [0, self.spec._y_offset, self.spec._z_offset]
-            )
 
             if not np.allclose(self.normal, np.array([-1, 0, 0])):
-                angle = trimesh.transformations.angle_between_vectors(
-                    np.array([-1, 0, 0]), self.normal
+                # Take new norm and normalize so x=1
+                # Apply two rotation matrices by the values of y and z after normalizing
+                norm = self.normal / self.normal[0]
+                rotation_matrix = trimesh.transformations.rotation_matrix(
+                    trimesh.transformations.angle_between_vectors(
+                        np.array([-1, 0, 0]), np.array([-1, norm[1], 0])
+                    ),
+                    np.array([0, 1, 0]),
                 )
-                axis = np.cross(np.array([-1, 0, 0]), self.normal)
-                rotation_matrix = trimesh.transformations.rotation_matrix(angle, axis)
                 mesh.apply_transform(rotation_matrix)
-            mesh.apply_translation(self.position)
+                rotation_matrix = trimesh.transformations.rotation_matrix(
+                    trimesh.transformations.angle_between_vectors(
+                        np.array([-1, 0, 0]), np.array([-1, 0, norm[2]])
+                    ),
+                    np.array([0, 0, 1]),
+                )
+                mesh.apply_transform(rotation_matrix)
+
+            mesh.apply_translation(
+                # normalize so bottom left corner is at [0, 0, 0]
+                extents / 2
+                # normalize so acoustic center is at [0, 0, 0]
+                - [0, self.spec._y_offset, self.spec._z_offset]
+                # move acoustic center into position
+                + self.position
+            )
 
             mesh.visual.vertex_colors = tv.random_color()  # pyright: ignore
             self._mesh = mesh